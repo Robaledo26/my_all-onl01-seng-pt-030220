96605073c59fc6a61f9703652a07217c89a329c2
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-14 01:19:41 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,11 +8,17 @@ def my_all?(collection)
  end
 end
 
-def my_all?
-  ([1,2,3])
-   i = 0 
-   while i < 3 
-   yield (1)
-   i = 0 + 1 
- end
+#def my_all?
+  #([1,2,3])
+  # i = 0 
+  # while i < 3 
+  # yield (1)
+  # i = 0 + 1 
+# end
+
+def my_alL? (collection)
+  i = 0 
+   block_return_va
+  lues = []
+  collection.length 
 end
