72d5482bdce8ab0bfbbd3720888ef2bafe2e9ed3
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-14 01:28:20 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,28 +1,5 @@
 require 'pry'
 
 def my_all?(collection)
- i = 0 
- while i < collections.legnth
-  yield(collection[i])
-  i += 1
- end
-end
 
-#def my_all?
-  #([1,2,3])
-  # i = 0 
-  # while i < 3 
-  # yield (1)
-  # i = 0 + 1 
-# end
-
-def my_alL? (collection)
-  i = 0 
-   block_return_values 
-  lues = []
-  while i < collection.length 
-  block_return_values << yield(collection[i])
-    i += 1 
-   end
-  end
 end
